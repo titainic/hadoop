9e59c399b6c041304fdf1f8c145d7f5a3ada1efd
titainic <binend@live.cn>
2015-11-09 19:53:47 +0800

    删除PairRDD中对两个RDD内链接,即获取两个RDD都有的元素,转换成一个新的RDD

--- com-hadoop-spark/src/main/java/com/spark/pairrdd/Join.java
+++ com-hadoop-spark/src/main/java/com/spark/pairrdd/Join.java
@@ -1,10 +1,16 @@
 package com.spark.pairrdd;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
- * Created by titanic on 15-11-9.
+ * 删除PairRDD中对两个RDD内链接,即获取两个RDD都有的元素,转换成一个新的RDD
  */
 public class Join
 {
@@ -22,9 +28,28 @@ public class Join
         JavaSparkContext jsc = new JavaSparkContext(conf);
         jsc.addJar("/home/titanic/soft/Work_Intellij/20151106/com-hadoop-spark/target/com-hadoop-spark-1.0-SNAPSHOT.jar");
 
+        List<Tuple2<Integer,Integer>> alist = new ArrayList<Tuple2<Integer, Integer>>();
+        List<Tuple2<Integer,Integer>> blist = new ArrayList<Tuple2<Integer, Integer>>();
 
+        for(int x = 0 ; x < 20; x++)
+        {
 
+            Tuple2 tx = new Tuple2(x,x);
+            alist.add(tx);
+        }
 
+        for(int x = 10 ; x < 30; x++)
+        {
+            Tuple2 t = new Tuple2(x,x);
+            blist.add(t);
+        }
+        JavaPairRDD<Integer,Integer> ardd = jsc.parallelizePairs(alist);
+        JavaPairRDD<Integer,Integer> brdd = jsc.parallelizePairs(blist);
+
+        JavaPairRDD<Integer, Tuple2<Integer, Integer>> crdd = ardd.join(brdd);
+
+        Map<Integer, Tuple2<Integer, Integer>> map = crdd.collectAsMap();
+        System.out.println(map);
 
     }
 }
