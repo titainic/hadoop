c359df416f243a84fbc58e8023f767141e51a1ab
titainic <binend@live.cn>
2015-11-09 12:52:04 +0800

    Fold

--- com-hadoop-spark/src/main/java/com/spark/rdd/Fold.java
+++ com-hadoop-spark/src/main/java/com/spark/rdd/Fold.java
@@ -1,40 +1,50 @@
-//package com.spark.rdd;
-//
-//import org.apache.spark.SparkConf;
-//import org.apache.spark.api.java.JavaRDD;
-//import org.apache.spark.api.java.JavaSparkContext;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///**
-// * Created by titanic on 15-10-26.
-// */
-//public class Fold
-//{
-////    public static void main(String[] args)
-////    {
-////        init();
-////    }
-//
-//    private static void init()
-//    {
-//
-//        SparkConf conf = new SparkConf();
-//        conf.setAppName("binend fold");
-//        conf.setMaster("spark://binend:7077");
-//
-//        JavaSparkContext jsc = new JavaSparkContext(conf);
-//        jsc.addJar("/home/titanic/soft/WorkSpace_intellij/hadoop-spark/com-hadoop-spark/target/com-hadoop-spark-1.0-SNAPSHOT.jar");
-//
-//        List<Integer> aList = new ArrayList<Integer>();
-//
-//        for(int i = 0 ; i <= 30; i++)
-//        {
-//            aList.add(i);
-//        }
-//
-//       JavaRDD<Integer> rdd = jsc.parallelize(aList);
-//
-//    }
-//}
+package com.spark.rdd;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by titanic on 15-10-26.
+ */
+public class Fold
+{
+    public static void main(String[] args)
+    {
+        init();
+    }
+
+    private static void init()
+    {
+
+        SparkConf conf = new SparkConf();
+        conf.setAppName("binend fold");
+        conf.setMaster("spark://localhost:7077");
+
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        jsc.addJar("/home/titanic/soft/Work_Intellij/20151106/com-hadoop-spark/target/com-hadoop-spark-1.0-SNAPSHOT.jar");
+
+        List<Integer> aList = new ArrayList<Integer>();
+
+        for(int i = 0 ; i <= 30; i++)
+        {
+            aList.add(i);
+        }
+
+       JavaRDD<Integer> rdd = jsc.parallelize(aList);
+
+       int x = rdd.fold(1, new Function2<Integer, Integer, Integer>()
+       {
+           public Integer call(Integer integer, Integer integer2) throws Exception
+           {
+               return null;
+           }
+       });
+        System.out.println(x);
+
+    }
+}
