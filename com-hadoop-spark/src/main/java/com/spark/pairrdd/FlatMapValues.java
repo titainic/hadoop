eec78537c22afeaec7ce4dafa50d3ef57458beba
titainic <binend@live.cn>
2015-11-11 11:47:21 +0800

    对pairRDD中每个值运用一个范湖迭代器的函数，然后对返回的每个元素都生成一个原键的键值对，通常用于符号化

--- com-hadoop-spark/src/main/java/com/spark/pairrdd/FlatMapValues.java
+++ com-hadoop-spark/src/main/java/com/spark/pairrdd/FlatMapValues.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 /**
- * Created by titanic on 15-11-9.
+ * 对pairRDD中每个值运用一个范湖迭代器的函数，然后对返回的每个元素都生成一个原键的键值对，通常用于符号化
  */
 public class FlatMapValues
 {
@@ -27,13 +27,13 @@ public class FlatMapValues
     {
         SparkConf conf = new SparkConf();
         conf.setAppName("FlatMapValues");
-        conf.setMaster("spark://localhost:7077");
+        conf.setMaster("local");
 
         JavaSparkContext jsc = new JavaSparkContext(conf);
 
-        jsc.addJar("/home/titanic/soft/Work_Intellij/20151106/com-hadoop-spark/target/com-hadoop-spark-1.0-SNAPSHOT.jar");
+        //jsc.addJar("/home/titanic/soft/Work_Intellij/20151106/com-hadoop-spark/target/com-hadoop-spark-1.0-SNAPSHOT.jar");
 
-        JavaRDD<String> dataRDD = jsc.textFile("file:///home/titanic/soft/spark-1.5.0/README.md");
+        JavaRDD<String> dataRDD = jsc.textFile("file:///D:\\soft\\spark\\spark-1.5.1-bin-hadoop2.6\\spark-1.5.1-bin-hadoop2.6\\README.md");
         JavaRDD<String> pairRDD = dataRDD.flatMap(new FlatMapFunction<String, String>()
         {
             public Iterable<String> call(String s) throws Exception
