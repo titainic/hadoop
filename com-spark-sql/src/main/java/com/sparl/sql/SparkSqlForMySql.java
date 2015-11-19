27387b6545f7f143de0d5b39831b490ffb5d7153
titainic <binend@live.cn>
2015-11-19 18:54:06 +0800

    spark链接mysql

--- com-spark-sql/src/main/java/com/sparl/sql/SparkSqlForMySql.java
+++ com-spark-sql/src/main/java/com/sparl/sql/SparkSqlForMySql.java
@@ -1,7 +1,14 @@
 package com.sparl.sql;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * spark链接mysql数据库
@@ -17,19 +24,26 @@ public class SparkSqlForMySql
 
     private static void init()
     {
-        Connection conn = null;
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("SparkSqlForMySql");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        SQLContext sqlctx = new SQLContext(jsc);
 
-        String url = "jdbc:mysql://localhost:3306/javademo?"
-                + "user=root&password=root&useUnicode=true&characterEncoding=UTF8";
+        Map<String, String> jdbcUrlParam = new HashMap<String, String>();
+        jdbcUrlParam.clear();
+        jdbcUrlParam.put("url","jdbc:mysql://10.101.235.37:3306/spark?user=spark&password=123456&useUnicode=true&characterEncoding=UTF8");
+        jdbcUrlParam.put("driver","com.mysql.jdbc.Driver");
+        jdbcUrlParam.put("dbtable", "spark_info_demo");
 
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-//            conn = DriverManager.getConnection();
-        } catch (ClassNotFoundException e)
-        {
-            e.printStackTrace();
-        }
+        DataFrame jdbcMysqlDF = sqlctx.read().format("jdbc").options(jdbcUrlParam).load();
+
+        jdbcMysqlDF.show();
+
+        jdbcMysqlDF.select(jdbcMysqlDF.col("id"),jdbcMysqlDF.col("name"),jdbcMysqlDF.col("gender"),jdbcMysqlDF.col("address")).show();
+
+        jsc.stop();
+
+
     }
+
 
 }
