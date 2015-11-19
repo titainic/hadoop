3bfd8648ded0124fb57370ef27cf9893f7feab69
titainic <binend@live.cn>
2015-11-19 13:51:52 +0800

    DataFrame查找json文件

---
+++ com-spark-sql/src/main/java/com/sparl/sql/DataFrameDemo.java
@@ -0,0 +1,44 @@
+package com.sparl.sql;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.hive.HiveContext;
+
+import java.util.List;
+
+/**
+ * Created by wb-yangbin.d on 2015/11/19.
+ */
+public class DataFrameDemo
+{
+    public static void main(String[] args)
+    {
+        init();
+    }
+
+    private static void init()
+    {
+        SparkConf conf = new SparkConf().setAppName("DataFrame").setMaster("local");
+
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        SQLContext sqlctx = new SQLContext(jsc);
+
+        org.apache.spark.sql.DataFrame df = sqlctx.jsonFile("D:\\work\\intellij_20151110\\spark\\com-spark-sql\\src\\main\\resources\\testweet.json");
+
+        //把加载的数据注册为零时表，给个表名
+        df.registerTempTable("tweets");
+
+        org.apache.spark.sql.DataFrame sqlForTweets = sqlctx.sql("SELECT text, retweetCount FROM tweets ORDER BY retweetCount LIMIT 10");
+
+        List<Row> rowList = sqlForTweets.collectAsList();
+        sqlForTweets.show();
+
+        System.out.println(rowList.get(0).toString());
+
+        jsc.stop();
+
+    }
+}
