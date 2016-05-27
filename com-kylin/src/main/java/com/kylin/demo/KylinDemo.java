93f240bf7abd0a96ae7fbac2cd508e877b51daf8
titainic <binend@live.cn>
2016-05-27 09:43:23 +0800

    新增kylin模块

---
+++ com-kylin/src/main/java/com/kylin/demo/KylinDemo.java
@@ -0,0 +1,12 @@
+package com.kylin.demo;
+
+/**
+ * Created by titanic on 16-5-27.
+ */
+public class KylinDemo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("");
+    }
+}
