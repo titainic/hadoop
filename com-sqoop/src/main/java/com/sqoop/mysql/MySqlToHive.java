3a11c302b66586edb5ea2bd16c44929ae7b50245
titainic <binend@live.cn>
2016-05-30 09:17:35 +0800

    新增sqoop模块，计划使用sqoop2

---
+++ com-sqoop/src/main/java/com/sqoop/mysql/MySqlToHive.java
@@ -0,0 +1,12 @@
+package com.sqoop.mysql;
+
+/**
+ * Created by titanic on 16-5-30.
+ */
+public class MySqlToHive
+{
+    public static void main(String[] args)
+    {
+        System.out.println("MySqlToHive");
+    }
+}
---
+++ com-sqoop/src/main/java/com/sqoop/oracle/OracleToHive.java
@@ -0,0 +1,12 @@
+package com.sqoop.oracle;
+
+/**
+ * Created by titanic on 16-5-30.
+ */
+public class OracleToHive
+{
+    public static void main(String[] args)
+    {
+        System.out.println("OracleToHive");
+    }
+}
