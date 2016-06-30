003854ef67b9355052e1dc98ff985a3ed90e3897
杨彬 <binend@live.cn>
2016-06-30 17:01:45 +0800

    代码优化

--- com-spark-es/src/main/java/com/titanic/spar2es/Spark2EsMap.java
+++ com-spark-es/src/main/java/com/titanic/spar2es/Spark2EsMap.java
@@ -7,7 +7,6 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
