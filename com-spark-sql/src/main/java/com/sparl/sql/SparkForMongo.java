146d5fd33f0bd1ed0945b4228dc524fa20355498
杨彬 <binend@live.cn>
2016-07-25 09:28:31 +0800

    有时间在写

---
+++ com-spark-sql/src/main/java/com/sparl/sql/SparkForMongo.java
@@ -0,0 +1,12 @@
+package com.sparl.sql;
+
+/**
+ * spark加载mongo
+ *
+ * https://github.com/mongodb/mongo-hadoop/wiki/Spark-Usage
+ * https://docs.mongodb.com/ecosystem/tools/hadoop/#hadoop-connector
+ *
+ */
+public class SparkForMongo
+{
+}
