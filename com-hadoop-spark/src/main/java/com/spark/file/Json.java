9bac9a119b7632af167dfafb7ff7d3fd603d61c2
杨彬 <binend@live.cn>
2017-05-03 10:55:22 +0800

    更新项目错误

--- com-hadoop-spark/src/main/java/com/spark/file/Json.java
+++ com-hadoop-spark/src/main/java/com/spark/file/Json.java
@@ -1,60 +1,60 @@
-package com.spark.file;
-
-import org.apache.avro.ipc.specific.Person;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import parquet.org.codehaus.jackson.JsonParser;
-import parquet.org.codehaus.jackson.map.MappingIterator;
-import parquet.org.codehaus.jackson.map.ObjectMapper;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-/**
- * spark中操作json文件。有问题。带解决
- */
-public class Json
-{
-    public static void main(String[] args)
-    {
-        init();
-    }
-
-    private static void init()
-    {
-        SparkConf conf = new SparkConf();
-        conf.setMaster("local");
-        conf.setAppName("Json");
-
-        JavaSparkContext jsc = new JavaSparkContext(conf);
-
-
-    }
-
-    class ParseJson implements FlatMapFunction<Iterator<String>,Person>
-    {
-        @Override
-        public Iterable<Person> call(Iterator<String> lines) throws Exception
-        {
-            List<Person> list = new ArrayList<Person>();
-            ObjectMapper mapper = new ObjectMapper();
-            while(lines.hasNext())
-            {
-                String line = lines.next();
-                try
-                {
-//                    mapper.readValues(line, Person.class);
-//                   list.add();
-                } catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-
-
-            }
-            return null;
-        }
-    }
-}
+//package com.spark.file;
+//
+//import org.apache.avro.ipc.specific.Person;
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.api.java.JavaSparkContext;
+//import org.apache.spark.api.java.function.FlatMapFunction;
+//import parquet.org.codehaus.jackson.JsonParser;
+//import parquet.org.codehaus.jackson.map.MappingIterator;
+//import parquet.org.codehaus.jackson.map.ObjectMapper;
+//
+//import java.util.ArrayList;
+//import java.util.Iterator;
+//import java.util.List;
+//
+///**
+// * spark中操作json文件。有问题。带解决
+// */
+//public class Json
+//{
+//    public static void main(String[] args)
+//    {
+//        init();
+//    }
+//
+//    private static void init()
+//    {
+//        SparkConf conf = new SparkConf();
+//        conf.setMaster("local");
+//        conf.setAppName("Json");
+//
+//        JavaSparkContext jsc = new JavaSparkContext(conf);
+//
+//
+//    }
+//
+//    class ParseJson implements FlatMapFunction<Iterator<String>,Person>
+//    {
+//        @Override
+//        public Iterable<Person> call(Iterator<String> lines) throws Exception
+//        {
+//            List<Person> list = new ArrayList<Person>();
+//            ObjectMapper mapper = new ObjectMapper();
+//            while(lines.hasNext())
+//            {
+//                String line = lines.next();
+//                try
+//                {
+////                    mapper.readValues(line, Person.class);
+////                   list.add();
+//                } catch (Exception e)
+//                {
+//                    e.printStackTrace();
+//                }
+//
+//
+//            }
+//            return null;
+//        }
+//    }
+//}
--- com-spark-ml/src/main/java/spark/ml/kmeans/KMeansExampleCiyt.java
+++ com-spark-ml/src/main/java/spark/ml/kmeans/KMeansExampleCiyt.java
@@ -1,91 +1,91 @@
-package spark.ml.kmeans;
-
-import org.apache.spark.SparkContext;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.mllib.clustering.KMeans;
-import org.apache.spark.mllib.clustering.KMeansModel;
-import org.apache.spark.mllib.linalg.Vector;
-import org.apache.spark.mllib.linalg.Vectors;
-import org.apache.spark.sql.SparkSession;
-
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.List;
-
-/**
- * k-means聚类实现
- */
-public class KMeansExampleCiyt implements Serializable
-{
-    public static void main(String[] args)
-    {
-        KMeansExampleCiyt kmeans = new KMeansExampleCiyt();
-        kmeans.startKmeans();
-
-    }
-
-    private void startKmeans()
-    {
-        String dataPath = "/home/titanic/soft/intellij_work/hadoop/com-spark-ml/src/main/resources/k-means/city.txt";
-        SparkSession spark = SparkSession.builder().master("spark://titanic-Lenovo:7077").appName("k-means").getOrCreate();
-        SparkContext sc = spark.sparkContext();
-
-        sc.addJar("/home/titanic/soft/intellij_work/hadoop/com-spark-ml/target/com-spark-ml-0.0.1-SNAPSHOT.jar");
-        JavaRDD<String> cityRDD = spark.read().textFile("file://" + dataPath).javaRDD();
-
-        System.out.println(cityRDD.first());
-
-        JavaRDD<Vector> paresdRDD = cityRDD.map(new Function<String, Vector>()
-        {
-            public Vector call(String s) throws Exception
-            {
-                String[] sarray = s.split(",");
-
-                String[] xx = new String[2];
-
-                for (int i = 0; i <sarray.length ; i++)
-                {
-                    xx[0] = sarray[2];
-                    xx[1] = sarray[3];
-                }
-                double[] values = new double[2];
-
-                    values = Double.parseDouble(xx);
-
-                return Vectors.dense(values);
-            }
-        });
-
-        paresdRDD.cache();
-
-        int numClusters = 3; //预测分为3个簇类
-        int numIterations = 20; //迭代20次
-        int runs = 10; //运行10次，选出最优解
-
-        final KMeansModel clusters = KMeans.train(paresdRDD.rdd(), numClusters, numIterations, runs);
-
-        //计算测试数据分别属于那个簇类
-        List<String> resList = paresdRDD.map(new Function<Vector, String>()
-        {
-            public String call(Vector v) throws Exception
-            {
-                String d = v.toString() + " belong to cluster :" + clusters.predict(v);
-                return d;
-            }
-        }).collect();
-
-
-//        sc.parallelize(Seq<>)
-
-        spark.stop();
-    }
-
-    public <T> void print(Collection<T> c)
-    {
-        for (T t : c)
-        {
-            System.out.println(t.toString());
-        }
-    }
-}
+//package spark.ml.kmeans;
+//
+//import org.apache.spark.SparkContext;
+//import org.apache.spark.api.java.JavaRDD;
+//import org.apache.spark.api.java.function.Function;
+//import org.apache.spark.mllib.clustering.KMeans;
+//import org.apache.spark.mllib.clustering.KMeansModel;
+//import org.apache.spark.mllib.linalg.Vector;
+//import org.apache.spark.mllib.linalg.Vectors;
+//import org.apache.spark.sql.SparkSession;
+//
+//import java.io.Serializable;
+//import java.util.Collection;
+//import java.util.List;
+//
+///**
+// * k-means聚类实现
+// */
+//public class KMeansExampleCiyt implements Serializable
+//{
+//    public static void main(String[] args)
+//    {
+//        KMeansExampleCiyt kmeans = new KMeansExampleCiyt();
+//        kmeans.startKmeans();
+//
+//    }
+//
+//    private void startKmeans()
+//    {
+//        String dataPath = "/home/titanic/soft/intellij_work/hadoop/com-spark-ml/src/main/resources/k-means/city.txt";
+//        SparkSession spark = SparkSession.builder().master("spark://titanic-Lenovo:7077").appName("k-means").getOrCreate();
+//        SparkContext sc = spark.sparkContext();
+//
+//        sc.addJar("/home/titanic/soft/intellij_work/hadoop/com-spark-ml/target/com-spark-ml-0.0.1-SNAPSHOT.jar");
+//        JavaRDD<String> cityRDD = spark.read().textFile("file://" + dataPath).javaRDD();
+//
+//        System.out.println(cityRDD.first());
+//
+//        JavaRDD<Vector> paresdRDD = cityRDD.map(new Function<String, Vector>()
+//        {
+//            public Vector call(String s) throws Exception
+//            {
+//                String[] sarray = s.split(",");
+//
+//                String[] xx = new String[2];
+//
+//                for (int i = 0; i <sarray.length ; i++)
+//                {
+//                    xx[0] = sarray[2];
+//                    xx[1] = sarray[3];
+//                }
+//                double[] values = new double[2];
+//
+//                    values = Double.parseDouble(xx);
+//
+//                return Vectors.dense(values);
+//            }
+//        });
+//
+//        paresdRDD.cache();
+//
+//        int numClusters = 3; //预测分为3个簇类
+//        int numIterations = 20; //迭代20次
+//        int runs = 10; //运行10次，选出最优解
+//
+//        final KMeansModel clusters = KMeans.train(paresdRDD.rdd(), numClusters, numIterations, runs);
+//
+//        //计算测试数据分别属于那个簇类
+//        List<String> resList = paresdRDD.map(new Function<Vector, String>()
+//        {
+//            public String call(Vector v) throws Exception
+//            {
+//                String d = v.toString() + " belong to cluster :" + clusters.predict(v);
+//                return d;
+//            }
+//        }).collect();
+//
+//
+////        sc.parallelize(Seq<>)
+//
+//        spark.stop();
+//    }
+//
+//    public <T> void print(Collection<T> c)
+//    {
+//        for (T t : c)
+//        {
+//            System.out.println(t.toString());
+//        }
+//    }
+//}
