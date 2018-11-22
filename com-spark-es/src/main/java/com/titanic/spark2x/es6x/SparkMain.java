24987afa2c91582a3599b4c785b5ff8e2e8d44b0
杨彬 <binend@live.cn>
2018-11-22 14:44:13 +0800

    优化代码注释

--- com-spark-es/src/main/java/com/titanic/spark2x/es6x/SparkMain.java
+++ com-spark-es/src/main/java/com/titanic/spark2x/es6x/SparkMain.java
@@ -5,8 +5,17 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.elasticsearch.spark.sql.api.java.JavaEsSparkSQL;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 要把elasticsearch-hadoop.6.4.3.jar拷贝到$SPARK_HOM/jars/下面
+ */
 public class SparkMain
 {
+    public static String url = "jdbc:oracle:thin:@//192.168.1.106:1521/orclpdb";
+    public static String driver = "oracle.jdbc.driver.OracleDriver";
+
     public static void main(String[] args)
     {
         SparkSession spark= SparkSession
@@ -21,6 +30,15 @@ public class SparkMain
         Dataset<Row>  data = JavaEsSparkSQL.esDF(spark, "base_bzxzf");
         data.show();
 
+        Map<String, String> db = new HashMap<String, String>();
+        db.put("url", url);
+        db.put("driver", driver);
+        db.put("user", "aps");
+        db.put("password" , "aps");
+        db.put("dbtable", "base_bzxzf");
+
+        Dataset<Row> ds = spark.read().format("jdbc").options(db).load();
+        ds.show();
 
 
         spark.stop();
