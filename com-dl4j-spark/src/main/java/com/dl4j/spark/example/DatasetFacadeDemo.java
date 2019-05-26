cc707eafb469265a0e01b5c1f67fd81b8f136c17
杨彬 <binend@live.cn>
2019-05-26 20:36:43 +0800

    提交新模块

---
+++ com-dl4j-spark/src/main/java/com/dl4j/spark/example/DatasetFacadeDemo.java
@@ -0,0 +1,20 @@
+package com.dl4j.spark.example;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.datavec.spark.transform.DataFrames;
+import org.datavec.spark.transform.DataRowsFacade;
+
+public class DatasetFacadeDemo
+{
+    public static void main(String[] args)
+    {
+        DataFrames dff = null;
+
+        Dataset<Row> ds =  null;
+//        DataRowsFacade df = DataFrames.;
+
+
+
+    }
+}
--- com-dl4j/src/main/java/com/dl4j/example/Dl4jExample.java
+++ com-dl4j/src/main/java/com/dl4j/example/Dl4jExample.java
@@ -1,10 +1,30 @@
 package com.dl4j.example;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.Arrays;
+
 public class Dl4jExample
 {
     public static void main(String[] args)
     {
-        System.out.println("qqqqqqqqq");
-        System.out.println("ssss");
+        //默认排序
+        INDArray defaultOrderNd = Nd4j.create(new double[][]{{1.0, 2.0,3.0}, {4.0, 5.0,6.0}});
+        double[] defaultOrderArray = defaultOrderNd.data().asDouble();
+        System.out.println(Arrays.toString(defaultOrderArray));
+
+
+        //c/c++排除（行的排序）
+        INDArray cOrderNd = Nd4j.create(new double[][]{{1.0, 2.0,3.0}, {4.0, 5.0,6.0}},'c');
+        double[] cOrderArray = cOrderNd.data().asDouble();
+        System.out.println(Arrays.toString(cOrderArray));
+
+        //c/c++排除（列的排序）
+        INDArray fOrderNd = Nd4j.create(new double[][]{{1.0, 2.0,3.0}, {4.0, 5.0,6.0}},'f');
+        double[] fOrderArray = fOrderNd.data().asDouble();
+        System.out.println(Arrays.toString(fOrderArray));
+
+
     }
 }
