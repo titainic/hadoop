6bdd9ca6d6f13d697a4c7447d38ba98cc81bce5a
杨彬 <binend@live.cn>
2019-07-05 01:09:14 +0800

    画图

--- com-dl4j/src/main/java/com/dl4j/example/PlotViewDemo.java
+++ com-dl4j/src/main/java/com/dl4j/example/PlotViewDemo.java
@@ -1,5 +1,7 @@
 package com.dl4j.example;
 
+import com.github.sh0nk.matplotlib4j.Plot;
+import com.github.sh0nk.matplotlib4j.PythonExecutionException;
 import org.jfree.chart.ChartColor;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
@@ -11,6 +13,8 @@ import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.ui.RefineryUtilities;
 
 import java.awt.*;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -18,7 +22,16 @@ public class PlotViewDemo
 {
     public static void main(String[] args)
     {
-
+        try
+        {
+            testPlot();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } catch (PythonExecutionException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void data(String title, List<Double> x, List<Double> y, double theta0, double theta1){
@@ -53,4 +66,21 @@ public class PlotViewDemo
         RefineryUtilities.centerFrameOnScreen(frame);
         frame.setVisible(true);
     }
+
+    // https://github.com/sh0nk/matplotlib4j
+    public static void testPlot() throws IOException, PythonExecutionException
+    {
+        Plot plt = Plot.create();
+        plt.plot()
+                .add(Arrays.asList(1.3, 2))
+                .label("label")
+                .linestyle("--");
+        plt.xlabel("xlabel");
+        plt.ylabel("ylabel");
+        plt.text(0.5, 0.2, "text");
+        plt.title("Title!");
+        plt.legend();
+        plt.show();
+    }
+
 }
