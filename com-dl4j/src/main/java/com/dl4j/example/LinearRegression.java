96e7004021e14aeb0474807a31ea3d9de22537f3
杨彬 <binend@live.cn>
2019-07-06 00:07:43 +0800

    线性回归调用matplot画图完成第一步

--- com-dl4j/src/main/java/com/dl4j/example/LinearRegression.java
+++ com-dl4j/src/main/java/com/dl4j/example/LinearRegression.java
@@ -1,11 +1,16 @@
 package com.dl4j.example;
 
+import com.github.sh0nk.matplotlib4j.Plot;
+import com.github.sh0nk.matplotlib4j.PythonConfig;
+import com.github.sh0nk.matplotlib4j.PythonExecutionException;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 
 public class LinearRegression
@@ -20,18 +25,23 @@ public class LinearRegression
 
 
 
-    public static void main(String[] args) throws IOException, InterruptedException
+    public static void main(String[] args) throws IOException, InterruptedException, PythonExecutionException
     {
         LinearRegression model = new LinearRegression();
 
+        //加载csv数据
         INDArray data = Nd4j.readNumpy("/home/titanic/soft/intellij_workspace/github-hadoop/com-dl4j/src/main/resources/lr2.csv", ",");
+//--------------------------------------------------------------------
+        //获取画图数据
+        INDArray xData= data.getColumn(0);
+        INDArray yData= data.getColumn(1);
+        double[] xDataPlot = xData.dup().data().asDouble();
+        double[] yDataPlot = yData.dup().data().asDouble();
+        List xList = arrat2List(xDataPlot);
+        List yList = arrat2List(yDataPlot);
+//-------------------------------------------------------------
 
-
-        data.getDouble(0, data.columns());
-
-
-//        System.out.println(sd);
-
+        //算法步骤
 
         //初始化
         double k_label = 1.1;
@@ -47,11 +57,17 @@ public class LinearRegression
             model.fitSGD(data, label);
         }
 
-        PlotViewDemo.data("lr",xList,yList,model.getK(),model.getB());
         System.out.println("k: " + model.getK());
         System.out.println("b: " + model.getB());
-
         System.out.println(label);
+//------------------------------------------------------------
+        //图形设置
+        Plot plt =  Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.plot().add(xList, yList,"o");//.add(Arrays.asList(model.getK(),model.getB()));////
+        plt.title("lr");
+        plt.legend();
+        plt.show();
+
     }
 
 
@@ -103,5 +119,22 @@ public class LinearRegression
         this.b = b;
     }
 
+    /**
+     * 为了图形化准备数据
+     * @param data
+     * @return
+     */
+    public static List<Double> arrat2List(double[] data)
+    {
+        List<Double> list = new ArrayList<Double>();
+        if (data != null)
+        {
+            for (int i = 0; i < data.length; i++)
+            {
+                list.add(Double.valueOf(data[i]));
+            }
+        }
+        return list;
+    }
 
 }
--- com-dl4j/src/main/java/com/dl4j/example/PlotUtil.java
+++ com-dl4j/src/main/java/com/dl4j/example/PlotUtil.java
@@ -1,241 +1,202 @@
 package com.dl4j.example;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.AxisLocation;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.block.BlockBorder;
-import org.jfree.chart.plot.DatasetRenderingOrder;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.GrayPaintScale;
-import org.jfree.chart.renderer.PaintScale;
-import org.jfree.chart.renderer.xy.XYBlockRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.chart.title.PaintScaleLegend;
-import org.jfree.data.xy.*;
-import org.jfree.ui.RectangleEdge;
-import org.jfree.ui.RectangleInsets;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.api.ops.impl.indexaccum.IMax;
-import org.nd4j.linalg.dataset.api.DataSet;
-import org.nd4j.linalg.factory.Nd4j;
+import com.github.sh0nk.matplotlib4j.NumpyUtils;
+import com.github.sh0nk.matplotlib4j.Plot;
+import com.github.sh0nk.matplotlib4j.PythonConfig;
+import com.github.sh0nk.matplotlib4j.PythonExecutionException;
+import com.github.sh0nk.matplotlib4j.builder.ContourBuilder;
+import com.github.sh0nk.matplotlib4j.builder.HistBuilder;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-/**Simple plotting methods for the MLPClassifier examples
+/**
+ * Simple plotting methods for the MLPClassifier examples
+ *
  * @author Alex Black
  */
-public class PlotUtil {
+public class PlotUtil
+{
 
-    /**Plot the training data. Assume 2d input, classification output
-     * @param features Training data features
-     * @param labels Training data labels (one-hot representation)
-     * @param backgroundIn sets of x,y points in input space, plotted in the background
-     * @param backgroundOut results of network evaluation at points in x,y points in space
-     * @param nDivisions Number of points (per axis, for the backgroundIn/backgroundOut arrays)
+    private static final boolean DRY_RUN = true;
+
+
+    public static void main(String[] args) throws IOException, PythonExecutionException
+    {
+        testPlotScatter();
+//        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+//        test();
+
+    }
+
+
+    public static void test() throws IOException, PythonExecutionException
+    {
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.plot()
+                .add(Arrays.asList(1.3, 20, 200, 300, 400, 1000), Arrays.asList(1, 4, 10, 20, 100, 800))
+                .label("label");
+//                .linestyle("-");
+
+
+
+        plt.xlabel("xlabel");
+        plt.ylabel("ylabel");
+        plt.text(0.5, 0.2, "text");
+        plt.title("Title!");
+        plt.legend();
+        plt.show();
+    }
+
+
+    public static void testPlotSin() throws IOException, PythonExecutionException {
+        List<Double> x = IntStream.range(0, 100).boxed()
+                .map(Integer::doubleValue)
+                .map(d -> d / 15).collect(Collectors.toList());
+        List<Double> y = x.stream().map(Math::sin).collect(Collectors.toList());
+
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.plot()
+                .add(x, y)
+                .label("sin")
+                .linestyle("--");
+        plt.xlim(1.0, 5.0);
+        plt.title("sin curve");
+        plt.legend().loc("upper right");
+        plt.show();
+    }
+
+    /**
+     *
+     * @throws IOException
+     * @throws PythonExecutionException
      */
-    public static void plotTrainingData(INDArray features, INDArray labels, INDArray backgroundIn, INDArray backgroundOut, int nDivisions){
-        double[] mins = backgroundIn.min(0).data().asDouble();
-        double[] maxs = backgroundIn.max(0).data().asDouble();
+    public static void testPlotScatter() throws IOException, PythonExecutionException {
 
-        XYZDataset backgroundData = createBackgroundData(backgroundIn, backgroundOut);
-        JPanel panel = new ChartPanel(createChart(backgroundData, mins, maxs, nDivisions, createDataSetTrain(features, labels)));
+        Plot plt =  Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.plot().add(Arrays.asList(1.3, 2, 20, 300, 400, 1000), Arrays.asList(1, 4, 10, 20, 100, 800),"o").add(Arrays.asList(1.3, 200));
 
-        JFrame f = new JFrame();
-        f.add(panel);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.pack();
-        f.setTitle("Training Data");
+//        plt.plot().add(Arrays.asList(1, 2, 3), Arrays.asList(1, -8, 27));
+        plt.title("scatter");
 
-        f.setVisible(true);
+        plt.show();
     }
 
-    /**Plot the training data. Assume 2d input, classification output
-     * @param features Training data features
-     * @param labels Training data labels (one-hot representation)
-     * @param predicted Network predictions, for the test points
-     * @param backgroundIn sets of x,y points in input space, plotted in the background
-     * @param backgroundOut results of network evaluation at points in x,y points in space
-     * @param nDivisions Number of points (per axis, for the backgroundIn/backgroundOut arrays)
-     */
-    public static void plotTestData(INDArray features, INDArray labels, INDArray predicted, INDArray backgroundIn, INDArray backgroundOut, int nDivisions){
+    public static void testPlotContour() throws IOException, PythonExecutionException {
+        List<Double> x = NumpyUtils.linspace(-1, 1, 100);
+        List<Double> y = NumpyUtils.linspace(-1, 1, 100);
+        NumpyUtils.Grid<Double> grid = NumpyUtils.meshgrid(x, y);
 
-        double[] mins = backgroundIn.min(0).data().asDouble();
-        double[] maxs = backgroundIn.max(0).data().asDouble();
+        List<List<Double>> zCalced = grid.calcZ((xi, yj) -> Math.sqrt(xi * xi + yj * yj));
 
-        XYZDataset backgroundData = createBackgroundData(backgroundIn, backgroundOut);
-        JPanel panel = new ChartPanel(createChart(backgroundData, mins, maxs, nDivisions, createDataSetTest(features, labels, predicted)));
-
-        JFrame f = new JFrame();
-        f.add(panel);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.pack();
-        f.setTitle("Test Data");
-
-        f.setVisible(true);
-
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        ContourBuilder contour = plt.contour().add(x, y, zCalced);
+        plt.clabel(contour).inline(true).fontsize(10);
+        plt.title("contour");
+        plt.legend().loc("upper right");
+        plt.show();
     }
 
 
-    /**Create data for the background data set
-     */
-    private static XYZDataset createBackgroundData(INDArray backgroundIn, INDArray backgroundOut) {
-        int nRows = backgroundIn.rows();
-        double[] xValues = new double[nRows];
-        double[] yValues = new double[nRows];
-        double[] zValues = new double[nRows];
-        for( int i=0; i<nRows; i++ ){
-            xValues[i] = backgroundIn.getDouble(i,0);
-            yValues[i] = backgroundIn.getDouble(i,1);
-            zValues[i] = backgroundOut.getDouble(i, 0);
+    public static void testPlotPColor() throws IOException, PythonExecutionException {
+        List<Double> x = NumpyUtils.linspace(-1, 1, 100);
+        List<Double> y = NumpyUtils.linspace(-1, 1, 100);
+        NumpyUtils.Grid<Double> grid = NumpyUtils.meshgrid(x, y);
 
-        }
+        List<List<Double>> cCalced = grid.calcZ((xi, yj) -> Math.sqrt(xi * xi + yj * yj));
 
-        DefaultXYZDataset dataset = new DefaultXYZDataset();
-        dataset.addSeries("Series 1",
-                new double[][]{xValues, yValues, zValues});
-        return dataset;
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.pcolor().add(x, y, cCalced).cmap("plt.cm.Blues");
+        plt.title("pcolor");
+        plt.legend().loc("upper right");
+        plt.show();
     }
 
-    //Training data
-    private static XYDataset createDataSetTrain(INDArray features, INDArray labels ){
-        int nRows = features.rows();
+    public static void testPlotOneHistogram() throws IOException, PythonExecutionException {
+        Random rand = new Random();
+        List<Double> x = IntStream.range(0, 1000).mapToObj(i -> rand.nextGaussian())
+                .collect(Collectors.toList());
 
-        int nClasses = 2; // Binary classification using one output call end sigmoid.
-
-            XYSeries[] series = new XYSeries[nClasses];
-        for( int i=0; i<series.length; i++) series[i] = new XYSeries("Class " + String.valueOf(i));
-        INDArray argMax = Nd4j.getExecutioner().exec(new IMax(labels, 1));
-        for( int i=0; i<nRows; i++ ){
-            int classIdx = (int)argMax.getDouble(i);
-            series[classIdx].add(features.getDouble(i, 0), features.getDouble(i, 1));
-        }
-
-        XYSeriesCollection c = new XYSeriesCollection();
-        for( XYSeries s : series) c.addSeries(s);
-        return c;
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.hist().add(x).orientation(HistBuilder.Orientation.horizontal);
+        plt.ylim(-5, 5);
+        plt.title("histogram");
+        plt.legend().loc("upper right");
+        plt.show();
     }
 
-    //Test data
-    private static XYDataset createDataSetTest(INDArray features, INDArray labels, INDArray predicted ){
-        int nRows = features.rows();
+    public static void testPlotTwoHistogram() throws IOException, PythonExecutionException {
+        Random rand = new Random();
+        List<Double> x1 = IntStream.range(0, 1000).mapToObj(i -> rand.nextGaussian())
+                .collect(Collectors.toList());
+        List<Double> x2 = IntStream.range(0, 1000).mapToObj(i -> 4.0 + rand.nextGaussian())
+                .collect(Collectors.toList());
 
-        int nClasses = 2; // Binary classification using one output call end sigmoid.
-
-        XYSeries[] series = new XYSeries[nClasses*nClasses];
-        int [] series_index = new int [] {0,3,2,1}; //little hack to make the charts look consistent.
-        for( int i=0; i<nClasses*nClasses; i++){
-            int trueClass = i/nClasses;
-            int predClass = i%nClasses;
-            String label = "actual=" + trueClass + ", pred=" + predClass;
-            series[series_index[i]] = new XYSeries(label);
-        }
-        INDArray actualIdx = labels.argMax(1);
-        INDArray predictedIdx = predicted.argMax(1);
-        for( int i=0; i<nRows; i++ ){
-            int classIdx = actualIdx.getInt(i);
-            int predIdx = predictedIdx.getInt(i);
-            int idx = series_index[classIdx * nClasses + predIdx];
-            series[idx].add(features.getDouble(i, 0), features.getDouble(i, 1));
-        }
-
-        XYSeriesCollection c = new XYSeriesCollection();
-        for( XYSeries s : series) c.addSeries(s);
-        return c;
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.hist().add(x1).add(x2).bins(20).stacked(true).color("#66DD66", "#6688FF").range(3, 5);
+        plt.xlim(-6, 10);
+        plt.title("histogram");
+        plt.legend().loc("upper right");
+        plt.show();
     }
 
-    private static JFreeChart createChart(XYZDataset dataset, double[] mins, double[] maxs, int nPoints, XYDataset xyData) {
-        NumberAxis xAxis = new NumberAxis("X");
-        xAxis.setRange(mins[0],maxs[0]);
+    public static void testPlotHistogramNoXError() throws IOException, PythonExecutionException {
 
-
-        NumberAxis yAxis = new NumberAxis("Y");
-        yAxis.setRange(mins[1], maxs[1]);
-
-        XYBlockRenderer renderer = new XYBlockRenderer();
-        renderer.setBlockWidth((maxs[0]-mins[0])/(nPoints-1));
-        renderer.setBlockHeight((maxs[1] - mins[1]) / (nPoints - 1));
-        PaintScale scale = new GrayPaintScale(0, 1.0);
-        renderer.setPaintScale(scale);
-        XYPlot plot = new XYPlot(dataset, xAxis, yAxis, renderer);
-        plot.setBackgroundPaint(Color.lightGray);
-        plot.setDomainGridlinesVisible(false);
-        plot.setRangeGridlinesVisible(false);
-        plot.setAxisOffset(new RectangleInsets(5, 5, 5, 5));
-        JFreeChart chart = new JFreeChart("", plot);
-        chart.getXYPlot().getRenderer().setSeriesVisibleInLegend(0, false);
-
-
-        NumberAxis scaleAxis = new NumberAxis("Probability (class 1)");
-        scaleAxis.setAxisLinePaint(Color.white);
-        scaleAxis.setTickMarkPaint(Color.white);
-        scaleAxis.setTickLabelFont(new Font("Dialog", Font.PLAIN, 7));
-        PaintScaleLegend legend = new PaintScaleLegend(new GrayPaintScale(),
-                scaleAxis);
-        legend.setStripOutlineVisible(false);
-        legend.setSubdivisionCount(20);
-        legend.setAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
-        legend.setAxisOffset(5.0);
-        legend.setMargin(new RectangleInsets(5, 5, 5, 5));
-        legend.setFrame(new BlockBorder(Color.red));
-        legend.setPadding(new RectangleInsets(10, 10, 10, 10));
-        legend.setStripWidth(10);
-        legend.setPosition(RectangleEdge.LEFT);
-        chart.addSubtitle(legend);
-
-        ChartUtilities.applyCurrentTheme(chart);
-
-        plot.setDataset(1, xyData);
-        XYLineAndShapeRenderer renderer2 = new XYLineAndShapeRenderer();
-        renderer2.setBaseLinesVisible(false);
-        plot.setRenderer(1, renderer2);
-
-        plot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
-
-        return chart;
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.hist();
+        plt.xlim(-5, 5);
+        plt.title("histogram");
+        plt.legend().loc("upper right");
+        plt.show();
     }
 
-    public static void plotDataset(ArrayList<DataSet> DataSetList){
+    public static void testNullCauseNoException() throws IOException, PythonExecutionException {
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.plot().add(Arrays.asList(1.3, 0x66, null))
+                .add(Arrays.asList(null, -3.2e-8, 1));
+        plt.show();
+    }
 
-        XYSeriesCollection c = new XYSeriesCollection();
+    public static void testShowTwiceClearFirstPlot() throws IOException, PythonExecutionException {
+        // TODO: Check .plot() or so is not called twice on the second run script
 
-        int dscounter = 1; //use to name the dataseries
-        for (DataSet ds : DataSetList)
-        {
-            INDArray features = ds.getFeatures();
-            INDArray outputs= ds.getLabels();
+        Plot plt = Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.plot().add(Arrays.asList(1.3, 2));
+        plt.title("Title!");
+        plt.legend();
+        plt.show();
 
-            int nRows = features.rows();
-            XYSeries series = new XYSeries("S" + dscounter);
-            for( int i=0; i<nRows; i++ ){
-                series.add(features.getDouble(i), outputs.getDouble(i));
-            }
+        plt.show();
+    }
 
-            c.addSeries(series);
-        }
+    public static void testSubplots() throws IOException, PythonExecutionException {
+        Plot plt =  Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
 
-        String title = "title";
-        String xAxisLabel = "xAxisLabel";
-        String yAxisLabel = "yAxisLabel";
-        PlotOrientation orientation = PlotOrientation.VERTICAL;
-        boolean legend = false;
-        boolean tooltips = false;
-        boolean urls = false;
-        JFreeChart chart = ChartFactory.createScatterPlot(title , xAxisLabel, yAxisLabel, c, orientation , legend , tooltips , urls);
-        JPanel panel = new ChartPanel(chart);
+//        plt.subplot(2, 1, 1);
+        plt.plot()
+                .add(Arrays.asList(1, 2 ), Arrays.asList(1, 4));
 
-        JFrame f = new JFrame();
-        f.add(panel);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.pack();
-        f.setTitle("Training Data");
+//        plt.subplot(2, 1, 2);
+        plt.plot()
+                .add(Arrays.asList(1, 2, 3), Arrays.asList(1, -8, 27));
 
-        f.setVisible(true);
+        plt.show();
+    }
+
+    public static void testThirdArgError() throws IOException, PythonExecutionException {
+
+
+        Plot plt =Plot.create(PythonConfig.pyenvConfig("anaconda3-4.6.11"));
+        plt.plot().add(Arrays.asList(1.3, 2))
+                .add(Arrays.asList(1.3, 2))
+                .add(Arrays.asList(1.3, 2))
+
+                .add(Arrays.asList(1.3, 2));
+        plt.show();
     }
 
 }
