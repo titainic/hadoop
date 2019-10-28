ea3ae8d560bf8ce2458628b8d4e61c16b106b91a
binend <binend@live.cn>
2019-10-28 20:15:25 +0800

    添加网络定型可视化

--- com-dl4j/src/main/java/com/dl4j/nn/demo/ShallowNeuralNetworkNN.java
+++ com-dl4j/src/main/java/com/dl4j/nn/demo/ShallowNeuralNetworkNN.java
@@ -1,6 +1,7 @@
 package com.dl4j.nn.demo;
 
 import com.dl4j.utils.DataInitUtils;
+import org.deeplearning4j.api.storage.StatsStorage;
 import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -10,6 +11,9 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.stats.StatsListener;
+import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
@@ -67,10 +71,25 @@ public class ShallowNeuralNetworkNN
 
         DataSetIterator train = getData();
 
+        //初始化用户界面后端
+        UIServer uiServer = UIServer.getInstance();
+
+        //设置网络信息（随时间变化的梯度、分值等）的存储位置。这里将其存储于内存。
+        StatsStorage statsStorage = new InMemoryStatsStorage();         //或者： new FileStatsStorage(File)，用于后续的保存和载入
+
+        //将StatsStorage实例连接至用户界面，让StatsStorage的内容能够被可视化
+        uiServer.attach(statsStorage);
+
+        //然后添加StatsListener来在网络定型时收集这些信息
+        net.setListeners(new StatsListener(statsStorage));
+
         for (int i = 0 ;i< 10000;i++)
         {
             net.fit(train);
         }
+
+
+
 
         viewPointModel(net);
 
