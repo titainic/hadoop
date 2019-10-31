cdc6c96c80f4809bd6c7d1f2186f99425e56f721
binend <binend@live.cn>
2019-10-31 19:49:45 +0800

    DL4J整合spark，未完成

--- com-dl4j-spark/src/main/java/com/dl4j/spark/demo/LinearRegressionNNSpark.java
+++ com-dl4j-spark/src/main/java/com/dl4j/spark/demo/LinearRegressionNNSpark.java
@@ -9,6 +9,7 @@ import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.spark.api.RDDTrainingApproach;
 import org.deeplearning4j.spark.api.TrainingMaster;
 import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
 import org.deeplearning4j.spark.parameterserver.training.SharedTrainingMaster;
@@ -29,22 +30,27 @@ public class LinearRegressionNNSpark
 
     public static void main(String[] args) throws IOException
     {
+//        CudaEnvironment.getInstance().getConfiguration().allowMultiGPU(true);
+
         INDArray data = Nd4j.readNumpy(path, ",");
         DataSet dataSet = new DataSet(data.getColumn(0).reshape(new int[]{41, 1}), data.getColumn(1).reshape(new int[]{41, 1}));
         List<DataSet> trainData = dataSet.asList();
 
 
-        SparkConf sparkConf = new SparkConf().setAppName("LinearRegressionNNSpark").setMaster("spark://titanic:7077");
+        SparkConf sparkConf = new SparkConf()
+                .setAppName("LinearRegressionNNSpark");
+//                .setMaster("spark://titanic:7077");
+//        .setMaster("local[*]");
         sparkConf.set("spark.hadoop.fs.defaultFS", "hdfs://titanic:8020");
-//        sparkConf.set("spark.kryo.registrator", "org.nd4j.Nd4jSerializer");
-        sparkConf.set("spark.sql.broadcastTimeout", "1200");
+        sparkConf.set("spark.kryo.registrator", "org.nd4j.Nd4jSerializer");
+//        sparkConf.set("spark.sql.broadcastTimeout", "1200");
 //        sparkConf.set("spark.driver.memory ", "4g");
 //        sparkConf.set("spark.executor.memory", "5g");
-//        sparkConf.set("spark.executor.extraJavaOptions", "-Dorg.bytedeco.javacpp.maxbytes=2921225472");
+        sparkConf.set("spark.executor.extraJavaOptions", "-Dorg.bytedeco.javacpp.maxbytes=2921225472");
 //        sparkConf.set()
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-        sc.addJar("/home/titanic/soft/intellij_workspace/github-hadoop/com-dl4j-spark/target/com-dl4j-spark-1.0-SNAPSHOT.jar");
+//        sc.addJar("/home/titanic/soft/intellij_workspace/github-hadoop/com-dl4j-spark/target/com-dl4j-spark-1.0-SNAPSHOT.jar");
 
         JavaRDD<DataSet> trainRDD = sc.parallelize(trainData);
 
@@ -66,7 +72,7 @@ public class LinearRegressionNNSpark
         net.init();
         VoidConfiguration conf = VoidConfiguration.builder()
                 .unicastPort(40123)             //Port that workers will use to communicate. Use any free port
-                .networkMask("10.168.0.0/24")     //Network mask for communication. Examples 10.0.0.0/24, or 192.168.0.0/16 etc
+                .networkMask("10.168.2.0")     //Network mask for communication. Examples 10.0.0.0/24, or 192.168.0.0/16 etc
                 .controllerAddress("10.168.2.177")  //IP of the master/driver
                 .build();
 
@@ -74,8 +80,10 @@ public class LinearRegressionNNSpark
                 .batchSizePerWorker(100) //Batch size for training
                 .updatesThreshold(1e-3)                 //Update threshold for quantization/compression. See technical explanation page
                 .workersPerNode(1)      // equal to number of GPUs. For CPUs: use 1; use > 1 for large core count CPUs
+                .rddTrainingApproach(RDDTrainingApproach.Direct)
 //                .meshBuildMode(MeshBuildMode.MESH)      // or MeshBuildMode.PLAIN for < 32 nodes
                 .build();
+
 
         SparkDl4jMultiLayer sparkNet = new SparkDl4jMultiLayer(sc, net, trainingMaster);
 
